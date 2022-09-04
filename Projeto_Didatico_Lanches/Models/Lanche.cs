354738eac6c2cff143a3e01d09cca2ee7dcd42e5
gbrb1 <gbrb1@hotmail.com>
2022-09-04 07:44:29 -0300

    Adicionando algumas data annotations no model de lanches

--- Projeto_Didatico_Lanches/Models/Lanche.cs
+++ Projeto_Didatico_Lanches/Models/Lanche.cs
@@ -1,9 +1,21 @@
-﻿namespace Projeto_Didatico_Lanches.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace Projeto_Didatico_Lanches.Models;
+[Table("Lanches")]
 public class Lanche
 {
+    [Key]
     public int LancheId { get; set; }
+
+    [Required(ErrorMessage = "O nome do lanche deve ser informado")]
+    [Display(Name = "Nome do Lanche")]
     public string Nome { get; set; }
+
+    [Required]
+    [Display(Name = "Descrição do Lanche")]
+    [MinLength(20, ErrorMessage = "Descrição deve ter no mínimo {1} caracteres")]
+    [MaxLength(200, ErrorMessage = "Descrição deve ter no máximo {1} caracteres")]
     public string DescricaoCurta { get; set; }
     public string DescricaoDetalhada { get; set; }
     public Decimal Preco { get; set; }
@@ -11,6 +23,7 @@ public class Lanche
     public string ImagemThumbnailUrl { get; set; }
     public bool IsLanchePreferido { get; set; }
     public bool EmEstoque { get; set; }
+    //[ForeignKey]
     public int CategoriaId { get; set; }
     public virtual Categoria Categoria { get; set; }
 }
