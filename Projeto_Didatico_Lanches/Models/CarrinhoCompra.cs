72f51a06a6271264d93525f2f83d375d72e59652
Gabriel Brum <gbrb1@hotmail.com>
2024-08-19 01:31:43 -0300

    implementando metodo de remoção do carrinho

--- Projeto_Didatico_Lanches/Models/CarrinhoCompra.cs
+++ Projeto_Didatico_Lanches/Models/CarrinhoCompra.cs
@@ -57,5 +57,27 @@ namespace Projeto_Didatico_Lanches.Models
             }
             _context.SaveChanges();
         }
+
+
+        public void RemoverDoCarrinho(Lanche lanche)
+        {
+            var carrinhoCompraItem = _context.CarrinhoCompraItens.SingleOrDefault(s => s.Lanche.LancheId == lanche.LancheId &&
+            s.CarrinhoCompraId == CarrinhoCompraId);
+
+            if (carrinhoCompraItem != null)
+            {
+
+                if (carrinhoCompraItem.Quantidade > 1)
+                {
+                    carrinhoCompraItem.Quantidade--;
+                }
+                else
+                {
+                    _context.CarrinhoCompraItens.Remove(carrinhoCompraItem);
+                }
+            }
+            _context.SaveChanges();
+
+        }
     }
 }
